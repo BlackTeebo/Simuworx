03005427c237ca54871ea624ac33931aebb00e7f
BlackVampire <m.kirches@gmx.de>
2013-11-08 10:47:46 +0100

    added main class

---
+++ Simuworx/src/simuworx/Simuworx.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package simuworx;
+
+/**
+ *
+ * @author michael
+ */
+public class Simuworx {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
