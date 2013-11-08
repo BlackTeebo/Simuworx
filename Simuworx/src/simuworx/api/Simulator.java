ac8ade5917fd3434ae0ee1f2bcd289c9e032118e
BlackVampire <m.kirches@gmx.de>
2013-11-08 11:46:35 +0100

    started project structure

---
+++ Simuworx/src/simuworx/api/Simulator.java
@@ -0,0 +1,16 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package simuworx.api;
+
+/**
+ *
+ * @author michael
+ */
+public abstract class Simulator extends Thread{
+    
+    @Override
+    public abstract void run();
+    
+}
