9b119a84a60813ba6f7a29a48469774e96073d4d
BlackVampire <m.kirches@gmx.de>
2013-11-11 18:13:24 +0100

    commited

---
+++ Simuworx/src/simuworx/logging/SimuworxLogger.java
@@ -0,0 +1,11 @@
+package simuworx.logging;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Teebo
+ * Date: 10.11.13
+ * Time: 19:56
+ * To change this template use File | Settings | File Templates.
+ */
+public class SimuworxLogger {
+}
